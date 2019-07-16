0a4af7963b301726a01644e15f324e0ac262187e
Martin Twum Mensah <martint.mensah@gmail.com>
2019-07-16 12:11:51 +0000

    feature: Add logger and errorHandler methods

--- api/server.js
+++ api/server.js
@@ -3,9 +3,27 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.status(200).send('<h2>Welcome to cars api</h2>');
 });
+
+function logger(req, res, next) {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} request from ${req.url}`
+  );
+  next();
+}
+
+function errorHandler(error, req, res, next) {
+  console.error('ERROR:', error);
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack,
+  });
+}
+
+server.use(errorHandler);
 
 module.exports = server;
